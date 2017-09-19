a9b4fb55bb799d40d404fa27d978889d9aea5e8e
aglindsay03 <31869240+aglindsay03@users.noreply.github.com>
2017-09-19 13:18:09 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,7 +23,7 @@
 
 #node aglindsay03.puppetlabs.vm {
   # comment this out when does with the excersise
-  #notify { "This will only be enforced on the Linux Node": }
+  notify { "This will only be enforced on the Linux Node": }
   }
   
 node default {
