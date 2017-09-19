3451a43547c223403ef75d28fc8b8b18be4bc833
Ignacio Rustan <30328877+nachoman22@users.noreply.github.com>
2017-09-19 12:38:58 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,6 +28,6 @@ node default {
   
   # example code for the classroom
   include examples::puppetize
-  
-  notify { "This is the default message from the production environment": }
+  notify { "Hey there! I am ${::hostname}": }
+#  notify { "This is the default message from the production environment": }
 }
