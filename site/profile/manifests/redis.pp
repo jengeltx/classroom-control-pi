053c84df7a1445f3f4d6ce425097dbd91e25ff0a
Ignacio Rustan <30328877+nachoman22@users.noreply.github.com>
2017-09-20 10:25:33 -0300

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -5,7 +5,9 @@ class profile::redis {
   
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
-    
+    class { '::redis':
+    maxmemory => '10mb',
+    }
   }
   contain redis
 }
